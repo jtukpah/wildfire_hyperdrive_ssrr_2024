c1de5fa45b65ad5864978d5ad5b320cda51a085b
Benjamin Pyatski <pyatski.b@northeastern.edu>
2023-02-16 16:58:55 -0500

    adjusted calibration.py to take rgb camera, and images from /ximea/cube_data /imec/cube_data and /camera/raw_image

--- scripts/calibration.py
+++ scripts/calibration.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python3
+
 import cv2 as cv
+from cv_bridge import CvBridge
 import numpy as np
+import rospy
+import ros_numpy
 
 import PySimpleGUI as sg
+from hsi_driver.msg import DataCube
+from sensor_msgs.msg import Image
 
 from os import mkdir, chdir, scandir, environ
 import copy
 
-environ['PATH'] += "/home/river/catkin_ws/src/hsi_driver/scripts/"
-from get_calibration_images import CalibrationImages
 
 # Authored by Gary Lvov
 class Calibration():
     def __init__(self):
-        self.camera_driver = CalibrationImages()
+        self.frame1 = []
+        self.frame2 = []
+        self.frame3 = []
+
+        self.ximea_sub = rospy.Subscriber('/ximea/cube_data', DataCube, self.callback_ximea)
+        self.imec_sub = rospy.Subscriber('/imec/cube_data', DataCube, self.callback_imec)
+        self.alvium_sub = rospy.Subscriber('/camera/image_raw', Image, self.callback_alvium)
+
         self.create_folders()
         self.create_gui()
         
+    def callback_ximea(self, msg):
+        #recontructs data cube
+        ximea_cube = np.reshape(msg.data, (msg.width, msg.height, msg.lam))
+        self.frame1 = ximea_cube[:, :, 0]
+
+    def callback_imec(self, msg):
+        #reconstructs data cube
+        imec_cube = np.reshape(msg.data, (msg.width, msg.height, msg.lam))
+        self.frame2 = imec_cube[:, :, 0]
+
+    def callback_alvium(self, msg):
+        self.frame3 = ros_numpy.numpify(msg)
+        print(self.frame3.shape)
+
     def create_folders(self):
         try:
             mkdir("calibration_000")
@@ -34,6 +59,7 @@ class Calibration():
         chdir(self.next_name)
         mkdir("cam1")
         mkdir("cam2")
+        mkdir("cam3")
         chdir("../")
 
     def update_config(self):
@@ -47,7 +73,7 @@ class Calibration():
         sg.theme("DarkPurple")
 
         layout = [[sg.Text("Gary and Ben's Sick Stereo Calibration Helper Tool", size=(40, 1), justification='center', font='Helvetica 20')],
-              [sg.Image(filename='', key='cam1'), sg.Image(filename='', key='cam2')],
+              [sg.Image(filename='', key='cam1'), sg.Image(filename='', key='cam2'), sg.Image(filename='', key='cam3')],
               [sg.Button('Save Image', size=(10, 1), font='Helvetica 14'),]]
 
         window = sg.Window("Gary and Ben's Camera Calibration Helper Tool",
@@ -58,23 +84,30 @@ class Calibration():
         saved_count = 0
         
         while True:
-            frame1, frame2 = self.camera_driver.grab_images()
             event, values = window.read(timeout=20)
 
             if event == 'Save Image':
                 cv.imwrite(self.next_name + "/cam1/" + str(saved_count) + ".png", frame1)
-                cv.imwrite(self.next_name + "/cam2/" + str(saved_count) + ".png", frame2)
+                cv.imwrite(self.next_name + "/cam2/" + str(saved_count) + ".png", self.frame2)
+                cv.imwrite(self.next_name + "/cam2/" + str(saved_count) + ".png", self.frame3)
+            
                 saved_count += 1
             
             elif event == sg.WIN_CLOSED:
                 break
 
-            imgbytes1 = cv.imencode('.png', frame1)[1].tobytes() 
-            imgbytes2 = cv.imencode('.png', frame2)[1].tobytes()
+            imgbytes1 = cv.imencode('.png', self.frame1)[1].tobytes() 
+            imgbytes2 = cv.imencode('.png', self.frame2)[1].tobytes()
+            imgbytes3 = cv.imencode('.png', self.frame3)[1].tobytes()
             window['cam1'].update(data=imgbytes1)
             window['cam2'].update(data=imgbytes2)
+            window['cam3'].update(data=imgbytes3)
 
 if __name__ == '__main__':
-    
-    calib = Calibration()
+    rospy.init_node('Calibration_Imgs', anonymous=True)
+    try:
+        my_node = Calibration()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        my_node.shutdown()
 
